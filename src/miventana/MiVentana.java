8d3e6464d81db8687b899db982440d0192555cc2
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2024-01-25 13:43:42 -0600

    EJEMPLO VENTANA LAYOUT EN JAVA

---
+++ src/miventana/MiVentana.java
@@ -0,0 +1,31 @@
+package miventana;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MiVentana extends JFrame{
+    
+    public MiVentana(String titulo) {
+        super (titulo);
+        setSize(300, 300);
+        setLayout(new FlowLayout());
+        setLayout(new BorderLayout());
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel miPanel = new JPanel();
+        miPanel.setLayout(new FlowLayout());
+        add(miPanel, BorderLayout.CENTER);
+        
+        for (int i = 0; i < 10; i++) {
+            miPanel.add(new JButton("Boton" + i));
+            add(new JTextField("Texto"), BorderLayout.NORTH);
+            add(new JCheckBox("CheckBox"), BorderLayout.SOUTH);
+        }
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        MiVentana myWin = new MiVentana("My Window");       
+    }
+    
+}
